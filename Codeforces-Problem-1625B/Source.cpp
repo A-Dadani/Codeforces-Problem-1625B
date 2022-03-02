830c30bcda52f00219e013cc9c481c3f36c7d80f
UBoiii <der_uboiii@outlook.com>
2022-03-02 22:47:54 +0100

    Completely different approach. Much simpler and more optimized

--- Codeforces-Problem-1625B/Source.cpp
+++ Codeforces-Problem-1625B/Source.cpp
@@ -5,100 +5,47 @@
 
 #include <iostream>
 #include <vector>
-#include <utility>
 #include <algorithm>
 #include <string>
-
-struct Element
-{
-	Element() = default;
-	Element(unsigned int val, unsigned int firstOccurence)
-		:
-		val(val)
-	{
-		allOccurences.emplace_back(firstOccurence);
-	}
-	unsigned int val;
-	std::vector<unsigned int> allOccurences;
-};
-
-template<typename T, typename Functor>
-std::vector<unsigned int> GetAllIndecies(std::vector<T> input, Functor lambda)
-{
-	std::vector<unsigned int> result;
-	for (unsigned int i = 0; i < input.size(); ++i)
-	{
-		if (lambda(input[i])) result.emplace_back(i);
-	}
-	return result;
-}
-
-template<typename T>
-std::pair<T, T> GetBestPair(std::vector<T> indecies)
-{
-	std::pair<unsigned int, T> minIndexDiff(0, 0);
-	for (unsigned int i = 0; i < indecies.size() - 1; ++i)
-	{
-		T diff = indecies[i + 1] - indecies[i];
-		if (diff == 1) return std::pair<T, T>(indecies[i], indecies[i + 1]);
-		if (minIndexDiff.second > diff)
-		{
-			minIndexDiff.second = diff;
-			minIndexDiff.first = i;
-		}
-	}
-	return std::pair<T, T>(indecies[minIndexDiff.first], indecies[minIndexDiff.first + 1]);
-}
 
 int main()
 {
 	//Capture Data
 	unsigned short int nCases;
 	std::cin >> nCases;
-	for (unsigned short int i = 0; i < nCases; ++i)
+	while (nCases--)
 	{
-		unsigned int arrSize = 150000; //DEBUG
+		unsigned int arrSize;
 		std::vector<unsigned int> inputArr;
+		std::cin >> arrSize;
 		inputArr.reserve(arrSize);
-		for (unsigned int d = 0; d < 150000; ++d)
+		for (unsigned int i = 0; i < arrSize; ++i)
 		{
-			inputArr.emplace_back(d);
+			unsigned int temp;
+			std::cin >> temp;
+			inputArr.emplace_back(temp);
 		}
-		//std::vector<unsigned int> inputArr(150000, 69);
-		//unsigned int arrSize;
-		//std::vector<unsigned int> inputArr;
-		//std::cin >> arrSize;
-		//inputArr.reserve(arrSize);
-		//for (unsigned int j = 0; j < arrSize; ++j)
-		//{
-		//	unsigned int temp;
-		//	std::cin >> temp;
-		//	inputArr.emplace_back(temp);
-		//}
 		//Capture Finished
 
 		//Processing Data
-		std::vector<Element> elements;
-		unsigned int currMaxPossibleLength = 0; //If no pair of arrays exists this variable is going to store 0 which we'll substitute with a -1 for print later
-		for (unsigned int j = 0; j < arrSize; ++j)
+		std::vector<int> lastIndex(150000, -1);
+		unsigned int bestDist = 0;
+		for (unsigned int i = 0; i < arrSize; ++i)
 		{
-			auto firstOcc = std::find_if(elements.begin(), elements.end(), [&inputArr, j](Element e) { return (e.val == inputArr[j]); });
-			if (firstOcc == elements.end()) elements.emplace_back(inputArr[j], j);
-			else
+			if (lastIndex[inputArr[i]] != -1)
 			{
-				firstOcc->allOccurences.emplace_back(j);
+				bestDist = (bestDist == 0 
+					? i - lastIndex[inputArr[i]]
+					: std::min(bestDist, i - lastIndex[inputArr[i]]));
+
 			}
-		}
-		for (const auto& e : elements)
-		{
-			if (e.allOccurences.size() < 2) continue; //Skip if no pairs can be formed
-			std::pair<unsigned int, unsigned int> bestPair = GetBestPair(e.allOccurences); //We get the best pair for the current number
-			currMaxPossibleLength = std::max(currMaxPossibleLength, std::min(bestPair.first, bestPair.second) + arrSize - std::max(bestPair.first, bestPair.second));
+			lastIndex[inputArr[i]] = i;
+			if (bestDist == 1) break;
 		}
 		//Finished Processing
-		
-		//Printing Results
-		std::cout << (currMaxPossibleLength == 0 ? "-1" : std::to_string(currMaxPossibleLength)) << std::endl;
+
+		//Print Results
+		std::cout << (bestDist == 0 ? "-1" : std::to_string(arrSize - bestDist)) << std::endl;
 		//Finished Printing
 	}
 	return 0;
